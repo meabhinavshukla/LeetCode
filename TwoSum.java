d21be240c37095da4d666a025b1b8f5c4b59fb6f
Abhinav Shukla <meabhinavshukla@gmail.com>
2015-11-01 23:11:13 -0500

    Create TwoSum.java

---
+++ TwoSum.java
@@ -0,0 +1,33 @@
+/*
+Given an array of integers, find two numbers such that they add up to a specific target number.
+
+The function twoSum should return indices of the two numbers such that they add up to the target, 
+where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+
+You may assume that each input would have exactly one solution.
+
+Input: numbers={2, 7, 11, 15}, target=9
+Output: index1=1, index2=2
+*/
+
+public class Solution 
+{
+    public int[] twoSum(int[] arr, int sum) 
+    {
+        HashMap<Integer, Integer> elements = new HashMap<Integer, Integer>();
+		
+		
+		for(int i = 0; i < arr.length; i++)
+		{
+			if(elements.containsKey(sum - arr[i]))
+			{
+			  int[] result = {elements.get(sum - arr[i]) +1 , i+1};
+				return result;
+			}
+			else
+				elements.put(arr[i], i);
+		}
+		
+		return new int[0];
+		
+    }
